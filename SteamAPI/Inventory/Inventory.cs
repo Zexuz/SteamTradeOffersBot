750224552b35ee4a6dbbce8866d55db0cb80b21f
Robin Edbom <robin.edbom@gmail.com>
2017-08-16 00:46:09 +0200

    Removed unused classes

--- SteamAPI/Inventory/Inventory.cs
+++ SteamAPI/Inventory/Inventory.cs
@@ -1,154 +1,167 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using Newtonsoft.Json;
 
-namespace SteamAPI
+namespace SteamAPI.Inventory
 {
     public class Inventory
     {
-        public Item[] Items { get; set; }
-        public bool IsPrivate { get; private set; }
+        public Item[] Items { get; private set; }
         public bool IsGood { get; private set; }
+
 
         public Inventory()
         {
             IsGood = false;
         }
 
-        protected Inventory (InventoryResult apiInventory)
-        {            
-            Items = apiInventory.items;
-            IsPrivate = (apiInventory.status == "15");
-            IsGood = (apiInventory.status == "1");
+        protected Inventory(Item[] items, int resultSuccess)
+        {
+            IsGood = resultSuccess == 1;
+            Items = items;
         }
 
-        public Item GetItem(ulong id)
+        public static Inventory GetInventory(ulong steamId, SteamWeb steamWeb, int appId)
         {
-            // Check for Private Inventory
-            if (this.IsPrivate)
-                throw new InventoryException("Unable to access Inventory: Inventory is Private!");
+            try
+            {
+                var url = $"http://steamcommunity.com/inventory/{steamId}/{appId}/2?trading=1";
+                var rawResponse = steamWeb.Fetch(url, "GET", null, false);
+                var response = JsonConvert.DeserializeObject<InventoryResponse>(rawResponse);
 
-            return (Items == null ? null : Items.FirstOrDefault(item => item.Id == id));
+                var items = MapInventoryResponse(response);
+
+                return new Inventory(items, response.Success);
+            }
+            catch (Exception e)
+            {
+                return new Inventory(null, 0);
+            }
         }
 
-        public Item GetItemByOriginalId(ulong originalId)
+        private static Item[] MapInventoryResponse(InventoryResponse response)
         {
-            // Check for Private Inventory
-            if (this.IsPrivate)
-                throw new InventoryException("Unable to access Inventory: Inventory is Private!");
-
-            return (Items == null ? null : Items.FirstOrDefault(item => item.OriginalId == originalId));
-        }
-
-        public List<Item> GetItemsByDefindex(int defindex)
-        {
-            // Check for Private Inventory
-            if (this.IsPrivate)
-                throw new InventoryException("Unable to access Inventory: Inventory is Private!");
-
-            return Items.Where(item => item.Defindex == defindex).ToList();
-        }
-
-        public class Item
-        {
-            [JsonProperty("id")]
-            public ulong Id { get; set; }
-
-            [JsonProperty("original_id")]
-            public ulong OriginalId { get; set; }
-
-            [JsonProperty("defindex")]
-            public ushort Defindex { get; set; }
-
-            [JsonProperty("level")]
-            public byte Level { get; set; }
-
-            [JsonProperty("quality")]
-            public int Quality { get; set; }
-
-            [JsonProperty("quantity")]
-            public int RemainingUses { get; set; }
-
-            [JsonProperty("origin")]
-            public int Origin { get; set; }
-
-            [JsonProperty("custom_name")]
-            public string CustomName { get; set; }
-
-            [JsonProperty("custom_desc")]
-            public string CustomDescription { get; set; }
-
-            [JsonProperty("flag_cannot_craft")]
-            public bool IsNotCraftable { get; set; }
-
-            [JsonProperty("flag_cannot_trade")]
-            public bool IsNotTradeable { get; set; }
-
-            [JsonProperty("attributes")]
-            public ItemAttribute[] Attributes { get; set; }
-
-            [JsonProperty("contained_item")]
-            public Item ContainedItem { get; set; }
-        }
-
-        public class ItemAttribute
-        {
-            [JsonProperty("defindex")]
-            public ushort Defindex { get; set; }
-
-            [JsonProperty("value")]
-            public string Value { get; set; }
-
-            [JsonProperty("float_value")]
-            public float FloatValue { get; set; }
-
-            [JsonProperty("account_info")]
-            public AccountInfo AccountInfo { get; set; }
-        }
-
-        public class AccountInfo
-        {
-            [JsonProperty("steamid")]
-            public ulong SteamID { get; set; }
-
-            [JsonProperty("personaname")]
-            public string PersonaName { get; set; }
-        }
-
-        protected class InventoryResult
-        {
-            public string status { get; set; }
-
-            public Item[] items { get; set; }
-
-            public uint num_backpack_slots { get; set; }
+            var items = new Item[response.Assets.Length];
+            for (var index = 0; index < response.Assets.Length; index++)
+            {
+                var asset = response.Assets[index];
+                foreach (var description in response.Descriptions)
+                {
+                    if (asset.Classid != description.Classid || asset.Instanceid != description.Instanceid) continue;
+                    var item = new Item
+                    {
+                        Appid = description.Appid,
+                        Appnameid = description.Appnameid,
+                        AssetId = asset.Assetid,
+                        BackgroundColor = description.BackgroundColor,
+                        Classid = description.Classid,
+                        Commodity = description.Commodity,
+                        Currency = description.Currency,
+                        IconUrl = description.IconUrl,
+                        Instanceid = description.Instanceid,
+                        Marketable = description.Marketable,
+                        MarketHashName = description.MarketHashName,
+                        MarketName = description.MarketName,
+                        MarketTradableRestriction = description.MarketTradableRestriction,
+                        NameColor = description.NameColor,
+                        Tradable = description.Tradable,
+                        Type = description.Type
+                    };
+                    items[index] = item;
+                }
+            }
+            return items;
         }
 
         protected class InventoryResponse
         {
-            public InventoryResult result;
+            public Asset[] Assets { get; set; }
+            public Description[] Descriptions { get; set; }
+
+            [JsonProperty("total_inventory_count")]
+            public int Count { get; set; }
+
+            public int Success { get; set; }
         }
-
-        public class InventoryException : Exception
-        {
-            public InventoryException()
-            {
-
-            }
-
-            public InventoryException(string message)
-                : base(message)
-            {
-
-            }
-
-            public InventoryException(string message, Exception inner)
-                : base(message, inner)
-            {
-
-            }
-        }
+        
+      
+       
     }
+    public class Item : Description
+    {
+        public string AssetId { get; set; }
+    }
+    
+    public class Asset
+    {
+        [JsonProperty("appid")]
+        public string Appid { get; set; }
+
+        [JsonProperty("contextid")]
+        public string Contextid { get; set; }
+
+        [JsonProperty("assetid")]
+        public string Assetid { get; set; }
+
+        [JsonProperty("classid")]
+        public string Classid { get; set; }
+
+        [JsonProperty("instanceid")]
+        public string Instanceid { get; set; }
+
+        [JsonProperty("amount")]
+        public string Amount { get; set; }
+    }
+
+    public class Description
+    {
+        [JsonProperty("appid")]
+        public int Appid { get; set; }
+
+        [JsonProperty("classid")]
+        public string Classid { get; set; }
+
+        [JsonProperty("instanceid")]
+        public string Instanceid { get; set; }
+
+        [JsonProperty("currency")]
+        public int Currency { get; set; }
+
+        [JsonProperty("background_color")]
+        public string BackgroundColor { get; set; }
+
+        [JsonProperty("icon_url")]
+        public string IconUrl { get; set; }
+
+        [JsonProperty("tradable")]
+        public bool Tradable { get; set; }
+
+        [JsonProperty("name")]
+        public string Appnameid { get; set; }
+
+        [JsonProperty("name_color")]
+        public string NameColor { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("market_name")]
+        public string MarketName { get; set; }
+
+        [JsonProperty("market_hash_name")]
+        public string MarketHashName { get; set; }
+
+        [JsonProperty("commodity")]
+        public int Commodity { get; set; }
+
+        [JsonProperty("market_tradable_restriction")]
+        public int MarketTradableRestriction { get; set; }
+
+        [JsonProperty("marketable")]
+        public int Marketable { get; set; }
+    }
+
+
+   
+
+   
 }
